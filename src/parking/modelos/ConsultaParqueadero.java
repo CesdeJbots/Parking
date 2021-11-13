3f5c3f30a4d7f9f0044f51e04e11a39b586d02e1
omarv94 <omarvalencia9415@gmail.com>
2021-11-12 20:55:07 -0500

    Se crearon consultas de los modelos parqueaderos vehiculos

--- src/parking/modelos/ConsultaParqueadero.java
+++ src/parking/modelos/ConsultaParqueadero.java
@@ -5,10 +5,79 @@
  */
 package parking.modelos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Santiago
  */
-public class ConsultaParqueadero {
+public class ConsultaParqueadero  extends ModeloDB{
+     PreparedStatement consultaSQL;
+    ResultSet resultadoSQL;
+    
+    public boolean RegistrarParqueadero (Parqueadero parqueadero){
+        Connection conexion=conectarDB();
+        String query="INSERT INTO parqueadero (cedulas_libres,celdas_ocupadas,celdas_) "
+                + "VALUES (?,?,?,?,?,?)";
+        
+        try{
+            
+            consultaSQL=conexion.prepareStatement(query);
+            
+            consultaSQL.setInt(1,parqueadero.getCeldasLibres());
+            consultaSQL.setInt(2,parqueadero.getCeldasOcupadas());
+            consultaSQL.setInt(3,parqueadero.getCeldasTotales());
+            
+            int resultado=consultaSQL.executeUpdate();
+            
+            if(resultado>0){
+                return true;
+            }else{
+                return false;
+            }
+        
+        
+        }catch(SQLException error){
+            System.out.println("Upps.. "+ error);
+            return false;
+        }
+    }
+    public Parqueadero ConsultarParqueadero(String id_parking ){
+        
+        Connection conexion=conectarDB();
+        String query="SELECT * from parqueadero where celdas_libres";
+        
+            try{
+            
+            consultaSQL=conexion.prepareStatement(query);
+            
+            consultaSQL.setString(1,id_parking);
+            
+            resultadoSQL=consultaSQL.executeQuery();
+            
+            Parqueadero parqueaderos=new Parqueadero();
+            if(resultadoSQL.next()){
+                
+                parqueaderos.setCeldasLibres(resultadoSQL.getByte("celdas_libres"));
+                parqueaderos.setCeldasOcupadas(resultadoSQL.getByte("celdas_ocupadas"));
+                
+                
+                return parqueaderos;
+            
+            }else{
+                return null;
+            }
+            
+        }catch(SQLException error){
+            System.out.println("Upps.. "+ error);
+            return null;
+        }
+            
+            
+    }
+    
     
 }
--- src/parking/modelos/ConsultaVehiculos.java
+++ src/parking/modelos/ConsultaVehiculos.java
@@ -1,14 +1,114 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package parking.modelos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
- * @author 505
+ * @author aspire
  */
-public class ConsultaVehiculos {
+public class ConsultaVehiculos extends ModeloDB {
+
+     PreparedStatement consultaSQL;
+    ResultSet resultadoSQL;
     
+    public boolean RegistrarVehiculos (Vehiculos vehiculos){
+        Connection conexion=conectarDB();
+        String query="INSERT INTO vehiculos (placa,fecha,hora_ingreso) "
+                + "VALUES (?,?,?)";
+        
+        try{
+            
+            consultaSQL=conexion.prepareStatement(query);
+            
+            consultaSQL.setString(1,vehiculos.getPlaca());
+            consultaSQL.setString(2,vehiculos.getFecha());
+            consultaSQL.setString(3,vehiculos.getHoraIngreso());
+           
+            
+            int resultado=consultaSQL.executeUpdate();
+            
+            if(resultado>0){
+                return true;
+            }else{
+                return false;
+            }
+        
+        
+        }catch(SQLException error){
+            System.out.println("Upps.. "+ error);
+            return false;
+        }
+    }
+    
+    public Vehiculos ConsultarVehiculos (String placa ){
+        
+        Connection conexion=conectarDB();
+        String query="SELECT vehiculo_id from vehiculos where placa =?";
+        
+            try{
+            
+            consultaSQL=conexion.prepareStatement(query);
+            
+            consultaSQL.setString(1,placa);
+            
+            resultadoSQL=consultaSQL.executeQuery();
+            
+            Vehiculos vehiculos=new Vehiculos();
+            if(resultadoSQL.next()){
+                
+                vehiculos.setPlaca(resultadoSQL.getString("placa"));
+                vehiculos.setFecha(resultadoSQL.getString("fecha"));
+                vehiculos.setHoraIngreso(resultadoSQL.getString("hora_ingreso"));
+                
+                return vehiculos;
+            
+            }else{
+                return null;
+            }
+            
+        }catch(SQLException error){
+            System.out.println("Upps.. "+ error);
+            return null;
+        }
+            
+            
+    }
+    
+    public boolean ActualizarVehiculos (Vehiculos vehiculos){
+        Connection conexion=conectarDB();
+        String query="UPDATE INTO vehiculos (placa,fecha,hora_ingreso,hora_salida) "
+                + "VALUES (?,?,?)";
+        
+        try{
+            
+            consultaSQL=conexion.prepareStatement(query);
+            
+            consultaSQL.setString(1,vehiculos.getPlaca());
+            consultaSQL.setString(2,vehiculos.getFecha());
+            consultaSQL.setString(3,vehiculos.getHoraIngreso());
+            consultaSQL.setString(4,vehiculos.getHoraSalida());
+            
+            int resultado=consultaSQL.executeUpdate();
+            
+            if(resultado>0){
+                return true;
+            }else{
+                return false;
+            }
+        
+        
+        }catch(SQLException error){
+            System.out.println("Upps.. "+ error);
+            return false;
+        }
+    }
+
+
 }
